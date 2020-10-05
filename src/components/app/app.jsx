1761c939ba55777d5eede75c9b1da88ad2e29425
VyglD <VyglD@yandex.ru>
2020-10-05 12:50:24 +0300

    Routing of project was added

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Main from "../main/main";
+import Login from "../login/login";
+import Favorites from "../favorites/favorites";
+import Offer from "../offer/offer";
 
 const App = (props) => {
   const {placesCount} = props;
 
   return (
-    <Main placesCount={placesCount}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main placesCount={placesCount}/>
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+        <Route exact path="/offer/:id">
+          <Offer />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
