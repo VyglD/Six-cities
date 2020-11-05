2f071b144ea3392b959e708ef00a268ecc1264b9
VyglD <VyglD@yandex.ru>
2020-11-05 15:23:13 +0300

    Component PlacesEmpty has been added

--- src/components/places/places.jsx
+++ src/components/places/places.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PlacesEmpty from "../places-empty/places-empty";
 import PlacesOffers from "../places-offers/places-offers";
 import Map from "../map/map";
 import {offersType} from "../../types";
@@ -8,11 +9,16 @@ import withActiveItem from "../../hocs/with-active-item/with-active-item";
 const Places = (props) => {
   const {offers} = props;
 
+  const emptyTriger = offers.length === 0;
+
   return (
     <div className="cities">
       {
-        offers.length > 0
+        emptyTriger
           ? (
+            <PlacesEmpty />
+          )
+          : (
             <div className="cities__places-container container">
               <PlacesOffers
                 {...props}
@@ -24,19 +30,6 @@ const Places = (props) => {
                   />
                 </section>
               </div>
-            </div>
-          )
-          : (
-            <div className="cities__places-container cities__places-container--empty container">
-              <section className="cities__no-places">
-                <div className="cities__status-wrapper tabs__content">
-                  <b className="cities__status">No places to stay available</b>
-                  <p className="cities__status-description">
-                    We could not find any property available at the moment in Dusseldorf
-                  </p>
-                </div>
-              </section>
-              <div className="cities__right-section"></div>
             </div>
           )
 
