1ec2813dd5442cd4ea340f4de57689dbcf353dea
VyglD <VyglD@yandex.ru>
2020-11-09 20:37:12 +0300

    Tests for component PlacesOffers have been added

---
+++ src/components/places-offers/places-offers.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {Provider} from 'react-redux';
+import {MemoryRouter} from "react-router-dom";
+import renderer from "react-test-renderer";
+import {PlacesOffers} from "./places-offers";
+import {mockFunction, mockOffers, activeCity, SortType} from "../../mocks/mock-data";
+import {mockStore} from "../../mocks/mock-store";
+
+it(`Render correctly component PlacesOffers`, () => {
+  const component = renderer.create(
+      <Provider store={mockStore}>
+        <MemoryRouter>
+          <PlacesOffers
+            offers={mockOffers}
+            activeCity={activeCity}
+            activeSort={SortType.DEFAULT.value}
+            onActiveSortChange={mockFunction}
+            onActiveCardChange={mockFunction}
+          />
+        </MemoryRouter>
+      </Provider>
+  ).toJSON();
+
+  expect(component).toMatchSnapshot();
+});
