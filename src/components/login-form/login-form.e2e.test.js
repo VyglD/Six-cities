6e1f11f844b65be6378e16ff46e011ba09a46ad9
VyglD <VyglD@yandex.ru>
2020-11-11 12:05:45 +0300

    Tests for component LoginForm have been added

---
+++ src/components/login-form/login-form.e2e.test.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {configure, mount} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import {LoginForm} from "./login-form";
+
+configure({adapter: new Adapter()});
+
+it(`Checking correction of submit data`, () => {
+  const handleSumbitForm = jest.fn();
+
+  const wrapper = mount(
+      <LoginForm
+        logIn={handleSumbitForm}
+      />
+  );
+
+  wrapper.instance()._emailFieldRef.current.value = ``;
+  wrapper.instance()._passwordFieldRef.current.value = ``;
+  wrapper.find(`.login__submit`).simulate(`click`);
+  expect(handleSumbitForm).toHaveBeenCalledTimes(0);
+
+  wrapper.instance()._emailFieldRef.current.value = `randomValue`;
+  wrapper.instance()._passwordFieldRef.current.value = `randomValue`;
+  wrapper.find(`.login__submit`).simulate(`click`);
+  expect(handleSumbitForm).toHaveBeenCalledTimes(0);
+
+  wrapper.instance()._emailFieldRef.current.value = `random@mail`;
+  wrapper.find(`.login__submit`).simulate(`click`);
+  expect(handleSumbitForm).toHaveBeenCalledTimes(0);
+
+  wrapper.instance()._emailFieldRef.current.value = `@mail.ru`;
+  wrapper.find(`.login__submit`).simulate(`click`);
+  expect(handleSumbitForm).toHaveBeenCalledTimes(0);
+
+  wrapper.instance()._emailFieldRef.current.value = `random@mail.ru`;
+  wrapper.instance()._passwordFieldRef.current.value = ``;
+  wrapper.find(`.login__submit`).simulate(`click`);
+  expect(handleSumbitForm).toHaveBeenCalledTimes(0);
+
+  wrapper.instance()._emailFieldRef.current.value = `random@mail.ru`;
+  wrapper.instance()._passwordFieldRef.current.value = `randomValue`;
+  wrapper.find(`.login__submit`).simulate(`click`);
+  expect(handleSumbitForm).toHaveBeenCalledTimes(1);
+});
